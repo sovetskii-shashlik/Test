3497ad3d87e694c398c7ebe18ca639164957b02e
sovetskii-shashlik <kudrovmaksim29@gmail.com>
2026-07-24 15:36:18 +0300

    Create basics.lua

---
+++ basics.lua
@@ -0,0 +1,3 @@
+loadstring(game.HttpGet(game, "https://github.com/sovetskii-shashlik/Test/raw/main/FlingGui"))()
+loadstring(game.HttpGet(game, "https://github.com/sovetskii-shashlik/Test/raw/main/custombackpack"))()
+loadstring(game.HttpGet(game, "https://raw.githubusercontent.com/sovetskii-shashlik/Shashlik-executor-1.1/refs/heads/main/Шашлык%20машлык.txt"))()
