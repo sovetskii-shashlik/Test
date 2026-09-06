bedceede370fef1e94903c011422e4ecdc791d1a
sovetskii-shashlik <kudrovmaksim29@gmail.com>
2026-09-07 00:24:55 +0300

    Update AntiErr.lua

--- AntiErr.lua
+++ AntiErr.lua
@@ -1,1 +1,3 @@
 for _,c in getconnections(game:GetService("LogService").MessageOut) do c:Disconnect() end for _,c in getconnections(game:GetService("ScriptContext").Error) do c:Disconnect() end
+
+for _,c in getconnections(game:GetService("LogService").MessageOut) do c:Disconnect() end for _,c in getconnections(game:GetService("ScriptContext").Warn) do c:Disconnect() end
